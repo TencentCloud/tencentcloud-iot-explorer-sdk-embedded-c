df68098095cb85e3bb87634213e6be0d1b8ab339
yougaliu <yougaliu@tencent.com>
2020-01-13 11:32:25 +0800

    1. 同步代码Astyle 2. 同步修复的两处潜在风险问题。

--- sdk_src/utils/qcloud_iot_ca.c
+++ sdk_src/utils/qcloud_iot_ca.c
@@ -105,17 +105,19 @@ static const char *iot_https_ca_crt = \
 
 #endif
 
-const char *iot_ca_get() {
+const char *iot_ca_get()
+{
 #ifndef AUTH_WITH_NOTLS
-	return iot_ca_crt;
+    return iot_ca_crt;
 #else
     return NULL;
 #endif
 }
 
-const char *iot_https_ca_get() {
-#if ((!defined(AUTH_WITH_NOTLS)) && (defined OTA_USE_HTTPS)) 
-	return iot_https_ca_crt;
+const char *iot_https_ca_get()
+{
+#if ((!defined(AUTH_WITH_NOTLS)) && (defined OTA_USE_HTTPS))
+    return iot_https_ca_crt;
 #else
     return NULL;
 #endif
