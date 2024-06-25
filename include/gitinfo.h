910aad05093f44cbc94c474996571f835e5d98ff
hubertxxu <hubertxxu@tencent.com>
2024-06-25 16:37:55 +0800

    fix : fix ota download size big bug

--- include/gitinfo.h
+++ include/gitinfo.h
@@ -1,4 +1,4 @@
 #ifndef __GIT_INFO_H_
 #define __GIT_INFO_H_
-#define GIT_COMMIT_ID "9e17580d47af5117a577b0589312ee6f4e3c1bcb"
+#define GIT_COMMIT_ID "77c26bdb692c2fed2f5acf68027614992d8d6266"
 #endif
